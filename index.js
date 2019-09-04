d007011ef355210b8c18532bc386f2353844fba8
Shaik Khaja <khaja78630@gmail.com>
2019-09-04 22:39:57 +0530

    Service changes

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+const express = require('express');
+const processRouter = require('./processRouter.js');
+const app = express();
+
+app.use(express.json());
+
+app.use("/",processRouter);
+
+app.listen(3000,(req,res)=>{
+    console.log("server is running on port 3000");
+});
---
+++ processRouter.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+const path = require("path");
+const fs=require("fs");
+router.get("getEnvironment/:process",function(req,res){
+    if(req.params.process == "P1" || req.params.process == "P2"){
+        const filePath = path.join(__dirname, req.params.process+"/.env");
+              
+        let readerStream = fs.createReadStream(filePath);
+
+        let  result ={};
+        readerStream.on('data', function(chunk) {
+            console.log(chunk);
+            let chunkArray = chunk.toString().split("\r\n");
+            for(let i=0;i<chunkArray.length;i++)
+            {
+                let splitArray = chunkArray[i].toString().split("=") ;
+                result[splitArray[0].toString().trim()] = splitArray[1].toString().trim();
+            }
+            console.log(result);
+            res.send(result);
+        });
+
+        
+    }
+    else {
+        res.send("Data not found").status(400); 
+    }
+  
+});
+
+router.post("/setEnvironment/:process/:key/:value",function(req,res){
+
+    console.log("from post");
+
+    if(req.params.process == "P1" || req.params.process == "P2"){
+        const filePath = path.join(__dirname, req.params.process+"/.env");
+              
+        let readerStream = fs.createReadStream(filePath);
+
+        let  result ={};
+        readerStream.on('data', function(chunk) {
+            console.log(chunk);
+            let chunkArray = chunk.toString().split("\n");
+            console.log("chunkArray" +chunkArray);
+            for(let i=0;i<chunkArray.length;i++)
+            {
+                let splitArray = chunkArray[i].toString().split("=") ;
+                result[splitArray[0].toString().trim()] = splitArray[1].toString().trim();
+            }
+            console.log(result);
+            
+            for(let key in result){
+                if(key == req.params.key){
+                    result[key] = req.params.value;
+                }
+            }
+            
+            let wstream = fs.createWriteStream(filePath);
+            for(let key in result){
+                wstream.write(key+" "+"="+" "+result[key]+"\n");
+                                
+            }
+            wstream.end();
+
+            res.send(result).status(200);
+    });
+        
+
+        
+    }
+   
+});
+
+module.exports = router;
